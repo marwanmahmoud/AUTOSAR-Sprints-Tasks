9fc0acc7f699617122ad388c2d02bff048e1fb94
marwanmahmoud <37125252+marwanmahmoud@users.noreply.github.com>
2020-08-02 15:28:20 +0200

    Update SWC_SeatManager_Template.c

--- Sensor&IO Task 4/Part1/SWC_SeatManager_Template.c
+++ Sensor&IO Task 4/Part1/SWC_SeatManager_Template.c
@@ -12,21 +12,53 @@
 
 #include "Rte_SWC_SeatManager.h"
 -----------static function ---------
-Impl_StepMotorStepType drive_motor(Impl_SensorPositionType position,Impl_SensorWeightType weight)
+static Impl_StepMotorStepType AUTO_Adjuste (Impl_SensorPositionType POSITION,Impl_SensorWeightType WEIGHT)
 {
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	switch(POSITION)
+	case SENSOR_POSITION_STEP_0:
+	if(WEIGHT>60)
+	{
+		return MOTOR_STEP_PLUS
+		
+	}
+	break;
+	case SENSOR_POSITION_STEP_1
+	if(WEIGHT>80)
+	{
+		return MOTOR_STEP_PLUS;
+	}
+	else if (WEIGHT<=80&&WEIGHT>=60)
+	{
+		/* DO Nothing*/
+		
+	}
+	else {
+		
+		return MOTOR_STEP_MINUS;
+	}
+	break;
+	case  SENSOR_POSITION_STEP_2:
+	if (WEIGHT>100)
+	{
+		return MOTOR_STEP_PLUS;
+	}
+	else if (WEIGHT<=100&&WEIGHT>=80)
+	{
+		
+		/*Do Nothing */
+	}
+	else 
+	{
+		
+		return MOTOR_STEP_MINUS;
+	}
+	break;
+	case  SENSOR_POSITION_STEP_3:
+	if(WEIGHT<100)
+	{
+		return MOTOR_STEP_MINUS;
+	}
+	break; 
 }
 
 /**
@@ -46,9 +78,10 @@ void SeatManager_AutoHeight (void)
 	Impl_SensorWeightType weight;
 
 	/* Server Call Points */
-	status = Rte_Call_rpHeightMotor_Move(step);
-	status = Rte_Call_rpHeightSensor_GetPosition(&position);
-	status = Rte_Call_rpWeightSensor_GetWeight(&weight);
+	status = Rte_Call_rpHeightSensor_GetPosition(&POSITION);
+	status = Rte_Call_rpWeightSensor_GetWeight(&WEIGHT);
+	step = AUTO_Adjuste(POSITION,WEIGHT);
+   Rte_Call_rpHeightMotor_Move(step);
 	
 }
 
@@ -70,10 +103,10 @@ void SeatManager_AutoIncline (void)
 	Impl_SensorWeightType weight;
 
 	/* Server Call Points */
+	status = Rte_Call_rpInclineSensor_GetPosition(&POSITION);
+	status = Rte_Call_rpWeightSensor_GetWeight(&WEIGHT);
+	step = AUTO_Adjuste(POSITION,WEIGHT);
 	status = Rte_Call_rpInclineMotor_Move(step);
-	status = Rte_Call_rpInclineSensor_GetPosition(&position);
-	status = Rte_Call_rpWeightSensor_GetWeight(&weight);
-	
 }
 
 
@@ -94,9 +127,78 @@ void SeatManager_AutoSlide (void)
 	Impl_SensorWeightType weight;
 
 	/* Server Call Points */
+	status = Rte_Call_rpSlideSensor_GetPosition(&POSITION);
+	status = Rte_Call_rpWeightSensor_GetWeight(&WEIGHT);
+	step = AUTO_Adjuste(POSITION,WEIGHT);
 	status = Rte_Call_rpSlideMotor_Move(step);
-	status = Rte_Call_rpSlideSensor_GetPosition(&position);
-	status = Rte_Call_rpWeightSensor_GetWeight(&weight);
+	
+}
+/**
+ *
+ * Runnable SeatManager_SetHeight
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_HeightBtnState
+ *
+ */
+
+void SeatManager_SetHeight (void)
+{
+Std_ReturnType status;
+	Impl_MultiStateBtnType HeightBtnState;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_HeightBtnState(&HeightBtnState);
+	if(statues==RTE_E_OK&HeightBtnState==MULTI_STATE_BTN_PLUS)
+		 Rte_Call_rpHeightMotor_Move(MOTOR_STEP_PLUS);
+	 else if (statues==RTE_E_OK&HeightBtnState==MULTI_STATE_BTN_MINUS)
+		  Rte_Call_rpHeightMotor_Move(MOTOR_STEP_MINUS);
+}
+
+
+/**
+ *
+ * Runnable SeatManager_SetSlide
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_SlideBtnState
+ *
+ */
+
+void SeatManager_SetSlide (void)
+{
+	td_ReturnType status;
+	Impl_MultiStateBtnType inclineBtnState;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_inclineBtnState(&inclineBtnState);
+	if(statues==RTE_E_OK&SlideBtnState==MULTI_STATE_BTN_PLUS)
+		 Rte_Call_rpSlideMotor_Move(MOTOR_STEP_PLUS);
+	 else if (statues==RTE_E_OK&inclineBtnState==MULTI_STATE_BTN_MINUS)
+		  Rte_Call_rpSlideMotor_Move(MOTOR_STEP_MINUS);
 	
 }
 
+
+/**
+ *
+ * Runnable SeatManager_SetIncline
+ *
+ * Triggered By:
+ *  - DataReceivedEventImpl.DRE_rpSeatCtrlBtns_InclineBtnState
+ *
+ */
+
+void SeatManager_SetIncline (void)
+{
+	Std_ReturnType status;
+	Impl_MultiStateBtnType inclineBtnState;
+
+	/* Data Receive Points */
+	status = Rte_Read_rpSeatCtrlBtns_inclineBtnState(&inclineBtnState);
+	if(statues==RTE_E_OK&inclineBtnState==MULTI_STATE_BTN_PLUS)
+		 Rte_Call_rpInclineMotor_Move(MOTOR_STEP_PLUS);
+	 else if (statues==RTE_E_OK&inclineBtnState==MULTI_STATE_BTN_MINUS)
+		  Rte_Call_rpInlclineMotor_Move(MOTOR_STEP_MINUS);
+	
+}
