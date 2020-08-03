ecfede2c2acb709a2b11fedc41b34ea2809a3213
marwanmahmoud <37125252+marwanmahmoud@users.noreply.github.com>
2020-08-03 04:23:19 +0200

    Update SWC_IoHwAbs.c

--- Full AUTOSAR application Design/SWC_IoHwAbs.c
+++ Full AUTOSAR application Design/SWC_IoHwAbs.c
@@ -1,174 +1,244 @@
 /**
  *
- * \file SWC_IoHwAbs_Template.c
- * \brief Rte Component Template for AUTOSAR SWC: SWC_IoHwAbs
+ * \file IoHwAbs_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: IoHwAbs
  *
  * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
- * Generated on 7/22/2020 05:36 PM
+ * Generated on 7/14/2020 11:53 PM
  *
  * For any inquiries: hassan.m.farahat@gmail.com
  *
  */
 
-#include "Rte_SWC_IoHwAbs.h"
+#include "IoHwAbs.h"
+
+/* For reading from Position Sensors */
+#include "Adc.h"
+
+/* For reading from Weight Sesnor */
+#include "Spi.h"
+
+/* For driving Motors (height, slide, incline) */
+#include "Dio.h"
+
+/* Shall be replaced with Inter Runnable Variable */
+/* Internal variables to read Position sensors in one Adc Group */
+static Adc_ValueGroupType IoHwAb_PositionSensorsReadings[ADC_GR0_NUM_CHANNELS] =
+{
+   /* Height */
+   0,
+   /* Incline */
+   0,
+   /* Slide */
+   0
+};
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUGet_Height
+ * Runnable IoHwAbs_ECUGet_Height
  *
  * Triggered By:
  *  - OperationInvokedEventImpl.OIE_ppIOGetHeight_IOGet
  *
  */
 
-void RE_IOHwAbs_ECUGet_Height (Impl_PositionReadingType* position)
+void IoHwAbs_ECUGet_Height (IoPositionSensorReadingType* position)
 {
 	Std_ReturnType status;
 
+	status = Adc_ReadGroup(AdcConf_AdcGroup_PositionsSensorsGrp, IoHwAb_PositionSensorsReadings);
+	
+	if(status == E_OK)
+	{
+		/* IoPositionSensorReadingType has to be adjusted in arxml file */
+		/* as per Adc resolution "Adc_ValueGroupType can be uint8 or uint16" */
+		*position = (IoPositionSensorReadingType)IoHwAb_PositionSensorsReadings[0];
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUGet_Incline
+ * Runnable IoHwAbs_ECUGet_Incline
  *
  * Triggered By:
  *  - OperationInvokedEventImpl.OIE_ppIOGetIncline_IOGet
  *
  */
 
-void RE_IOHwAbs_ECUGet_Incline (Impl_PositionReadingType* position)
+void IoHwAbs_ECUGet_Incline (IoPositionSensorReadingType* position)
 {
 	Std_ReturnType status;
 
+	status = Adc_ReadGroup(AdcConf_AdcGroup_PositionsSensorsGrp, IoHwAb_PositionSensorsReadings);
+	
+	if(status == E_OK)
+	{
+		/* IoPositionSensorReadingType has to be adjusted in arxml file */
+		/* as per Adc resolution "Adc_ValueGroupType can be uint8 or uint16" */
+		*position = (IoPositionSensorReadingType)IoHwAb_PositionSensorsReadings[1];
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUGet_Slide
+ * Runnable IoHwAbs_ECUGet_Slide
  *
  * Triggered By:
  *  - OperationInvokedEventImpl.OIE_ppIOGetSlide_IOGet
  *
  */
 
-void RE_IOHwAbs_ECUGet_Slide (Impl_PositionReadingType* position)
+void IoHwAbs_ECUGet_Slide (IoPositionSensorReadingType* position)
 {
 	Std_ReturnType status;
 
+	status = Adc_ReadGroup(AdcConf_AdcGroup_PositionsSensorsGrp, IoHwAb_PositionSensorsReadings);
+	
+	if(status == E_OK)
+	{
+		/* IoPositionSensorReadingType has to be adjusted in arxml file */
+		/* as per Adc resolution "Adc_ValueGroupType can be uint8 or uint16" */
+		*position = (IoPositionSensorReadingType)IoHwAb_PositionSensorsReadings[2];
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUGet_Weight
+ * Runnable IoHwAbs_ECUGet_Weight
  *
  * Triggered By:
  *  - OperationInvokedEventImpl.OIE_ppIOGetWeight_IOGet
  *
  */
 
-void RE_IOHwAbs_ECUGet_Weight (Impl_WeightReadingType* weight)
+void IoHwAbs_ECUGet_Weight (IoWeightSensorReadingType* weight)
 {
 	Std_ReturnType status;
+	Spi_DataType spiData;
 
+	status = Spi_ReadIB(SpiConf_SpiChannel_WeightSensor, &spiData);
+	
+	if(status == E_OK)
+	{
+		/* IoWeightSensorReadingType has to be adjusted in arxml file */
+		/* as per Spi_DataType "can be uint8 or uint16" */
+		*weight = (IoWeightSensorReadingType)spiData;
+	}
+	else
+	{
+		/* Operation shall be updated to return failure error code */
+	}
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUSetBackword_Height
+ * Runnable IoHwAbs_ECUSetForward_Height
  *
  * Triggered By:
- *  - OperationInvokedEventImpl.OIE_ppIOSetHeight_IOSetbackword
+ *  - OperationInvokedEventImpl.OIE_ppIOSetHeight_IOSetForward
  *
  */
 
-void RE_IOHwAbs_ECUSetBackword_Height (void)
+void IoHwAbs_ECUSetForward_Height (void)
 {
-	Std_ReturnType status;
-
+	/* Write HIGH to move forward */
+	Dio_WriteChannel(DioConfg_DioChannel_HeightMotorCh, STD_HIGH); 
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUSetBackword_Incline
+ * Runnable IoHwAbs_ECUSetForward_Incline
  *
  * Triggered By:
- *  - OperationInvokedEventImpl.OIE_ppOSetIncline_IOSetbackword
+ *  - OperationInvokedEventImpl.OIE_ppIOSetIncline_IOSetForward
  *
  */
 
-void RE_IOHwAbs_ECUSetBackword_Incline (void)
+void IoHwAbs_ECUSetForward_Incline (void)
 {
-	Std_ReturnType status;
-
+	/* Write HIGH to move forward */
+	Dio_WriteChannel(DioConfg_DioChannel_InclineMotorCh, STD_HIGH); 
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUSetBackword_Slide
+ * Runnable IoHwAbs_ECUSetForward_Slide
  *
  * Triggered By:
- *  - OperationInvokedEventImpl.OIE_ppOSetIncline_IOSetbackword
+ *  - OperationInvokedEventImpl.OIE_ppIOSetSlide_IOSetForward
  *
  */
 
-void RE_IOHwAbs_ECUSetBackword_Slide (void)
+void IoHwAbs_ECUSetForward_Slide (void)
 {
-	Std_ReturnType status;
-
+	/* Write HIGH to move forward */
+	Dio_WriteChannel(DioConfg_DioChannel_SlideMotorCh, STD_HIGH); 
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUSetForword_Height
+ * Runnable IoHwAbs_ECUSetBackword_Height
  *
  * Triggered By:
- *  - OperationInvokedEventImpl.OIE_ppIOSetHeight_IOSetforword
+ *  - OperationInvokedEventImpl.OIE_ppIOSetHeight_IOSetBackword
  *
  */
 
-void RE_IOHwAbs_ECUSetForword_Height (void)
+void IoHwAbs_ECUSetBackword_Height (void)
 {
-	Std_ReturnType status;
-
+	/* Write LOW to move Backword */
+	Dio_WriteChannel(DioConfg_DioChannel_HeightMotorCh, STD_LOW); 
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUSetForword_Incline
+ * Runnable IoHwAbs_ECUSetBackword_Incline
  *
  * Triggered By:
- *  - OperationInvokedEventImpl.OIE_ppOSetIncline_IOSetforword
+ *  - OperationInvokedEventImpl.OIE_ppIOSetIncline_IOSetBackword
  *
  */
 
-void RE_IOHwAbs_ECUSetForword_Incline (void)
+void IoHwAbs_ECUSetBackword_Incline (void)
 {
-	Std_ReturnType status;
-
+	/* Write LOW to move Backword*/
+	Dio_WriteChannel(DioConfg_DioChannel_InclineMotorCh, STD_LOW); 
 }
 
 
 /**
  *
- * Runnable RE_IOHwAbs_ECUSetForword_Slide
+ * Runnable IoHwAbs_ECUSetBackword_Slide
  *
  * Triggered By:
- *  - OperationInvokedEventImpl.OIE_ppOSetSlide_IOSetforword
+ *  - OperationInvokedEventImpl.OIE_ppIOSetSlide_IOSetBackword
  *
  */
 
-void RE_IOHwAbs_ECUSetForword_Slide (void)
+void IoHwAbs_ECUSetBackword_Slide (void)
 {
-	Std_ReturnType status;
-
+	/* Write LOW to move Backword */
+	Dio_WriteChannel(DioConfg_DioChannel_SlideMotorCh, STD_LOW); 
 }
 
